5c66438692167a75b1973c65e09a358e1191e725
Ali-Z0 <ali.zoubir@bluewin.ch>
2026-04-30 08:59:41 +0200

    Use custom domain base path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/radon-sense/',
+  base: '/',
   plugins: [react(), tailwindcss()],
 })
